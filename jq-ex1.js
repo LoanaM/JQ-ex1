f665fd1d89a847dabce1af365380fe895e785c5d
LoanaWitch <loanawitch@live.com>
2018-04-03 22:49:48 +0200

    rifatto con show-hide

--- jq-ex1.js
+++ jq-ex1.js
@@ -1,6 +1,11 @@
 $('button').click(function() {
-  $('p').html('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum');
+  $('p').show("slow");
 });
+$('button').dblclick(function() {
+  $('p').hide(3000)
+});
+
+
 $('h1').on({
   mouseenter: function(){
     $('h1').css('background-color','yellow');
