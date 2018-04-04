309603cf42336db6bfa41e38975c53bb606bbd18
LoanaWitch <loanawitch@live.com>
2018-04-04 15:44:17 +0200

    ex1 modificato in classe

--- jq-ex1.js
+++ jq-ex1.js
@@ -1,15 +1,3 @@
-$('button').click(function() {
-  $('p').show("slow");
-});
-$('button').dblclick(function() {
-  $('p').hide(3000)
-});
-
-
-$('h1').on({
-  mouseenter: function(){
-    $('h1').css('background-color','yellow');
-  }, mouseleave: function(){
-    $('h1').css('background-color','red');
-  },
+$('.button').click(function(){
+  $(this).children('p').toggle()
 })
